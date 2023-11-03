60b21a7faaf2f2cc52f32ee68704f7a88690d929
SoyeonL <sylee932003@gmail.com>
2023-11-02 23:54:53 -0700

    Created product card component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import "./components/category-item/category-item.component";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
-  const Shop = () => {
-    return <h2>I'm the shop page</h2>;
-  };
-
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from "./contexts/products.context";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,7 +12,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
