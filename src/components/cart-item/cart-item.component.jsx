5bc6c448daa78cd4dcd2cbf33dd0f8ce1eada188
SoyeonL <sylee932003@gmail.com>
2023-11-14 18:41:54 -0800

    edited cart item styling with styled component

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,19 +1,19 @@
-import "./cart-item.styles.scss";
+import { ItemDetails, CartItemContainer } from "./cart-item.styles";
 
 const CartItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
 
   return (
-    <div className="cart-item-container">
+    <CartItemContainer>
       <img src={imageUrl} alt={`${name}`} />
-      <div className="item-details">
+      <ItemDetails>
         <span className="name">{name}</span>
         <span className="price">
           {quantity} x ${price}
         </span>
-      </div>
+      </ItemDetails>
       <span>{quantity}</span>
-    </div>
+    </CartItemContainer>
   );
 };
 
