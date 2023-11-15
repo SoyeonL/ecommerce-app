e80702895cc65c57b1346f2ea276e63190a4d8e2
SoyeonL <sylee932003@gmail.com>
2023-11-14 17:59:14 -0800

    edited directory item styling with styled component

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,19 +1,20 @@
-import "./directory-item.styles.scss";
+import {
+  BackgroundImage,
+  Body,
+  DirectoryItemContainer,
+} from "./directory-item.styles";
 
 const DirectoryItem = ({ category }) => {
   const { id, imageUrl, title } = category;
 
   return (
-    <div key={id} className="directory-item-container">
-      <div
-        className="background-image"
-        style={{ backgroundImage: `url(${imageUrl})` }}
-      />
-      <div className="directory-item-body">
+    <DirectoryItemContainer key={id}>
+      <BackgroundImage imageurl={imageUrl} />
+      <Body>
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
-    </div>
+      </Body>
+    </DirectoryItemContainer>
   );
 };
 
