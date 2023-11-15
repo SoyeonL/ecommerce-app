f2e4706fca58975fded2bb113dafd2b027071e61
SoyeonL <sylee932003@gmail.com>
2023-11-15 13:37:04 -0800

    linked directory items to correspoding routes

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import {
   BackgroundImage,
   Body,
@@ -5,10 +6,15 @@ import {
 } from "./directory-item.styles";
 
 const DirectoryItem = ({ category }) => {
-  const { id, imageUrl, title } = category;
+  const { id, imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate(route);
+  };
 
   return (
-    <DirectoryItemContainer key={id}>
+    <DirectoryItemContainer key={id} onClick={navigateHandler}>
       <BackgroundImage imageurl={imageUrl} />
       <Body>
         <h2>{title}</h2>
