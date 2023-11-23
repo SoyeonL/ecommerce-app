09879e0a5f2129ea3340a82baedfe4a6de150198
SoyeonL <sylee932003@gmail.com>
2023-11-23 15:37:48 -0800

    extracted currentUser from the redux store using useSelector

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,8 @@
 import { Fragment, useContext } from "react";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectCurrrentUser } from "../../store/user/user.selector.js";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
-import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
@@ -14,10 +15,8 @@ import {
 } from "./navigation.styles.jsx";
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrrentUser);
   const { isCartOpen } = useContext(CartContext);
-
-  console.log(currentUser);
 
   return (
     <Fragment>
