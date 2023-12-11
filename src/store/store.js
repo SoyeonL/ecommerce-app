9dc44c64eccc6f6b983738ab18b5dabe56dd3287
SoyeonL <sylee932003@gmail.com>
2023-12-11 14:47:02 -0800

    moved our logger into middleware folder

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,9 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
+
 import { rootReducer } from "./root-reducer";
-
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log("type", action.type);
-  console.log("payload", action.payload);
-  console.log("current state", store.getState());
-
-  next(action);
-
-  console.log("next state", store.getState());
-};
 
 const persistConfig = {
   key: "root",
@@ -25,7 +13,9 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware];
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
