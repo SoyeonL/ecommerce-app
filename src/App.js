fe6c1721d75d8390f07efc5845b6dcf9b05e1853
SoyeonL <sylee932003@gmail.com>
2023-12-15 23:52:14 -0800

    converting onAuthStateChangedListener to Promise

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Checkout from "./routes/checkout/checkout.component";
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
+  getCurrentUser,
 } from "./utils/firebase/firebase.utils";
 import { setCurrentUser } from "./store/user/user.action";
 
@@ -16,14 +17,7 @@ const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      dispatch(setCurrentUser(user));
-    });
-
-    return unsubscribe;
+    getCurrentUser().then((user) => console.log(user));
   }, []);
 
   return (
--- src/store/store.js
+++ src/store/store.js
@@ -11,7 +11,7 @@ import { rootReducer } from "./root-reducer";
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["cart"],
+  blacklist: ["user"],
 };
 
 const sagaMiddleware = createSagaMiddleware();
